6b663f040459ed3f9d8a16a9535f56a867cad093
mmanvi01 <96523859+mmanvi01@users.noreply.github.com>
2022-01-15 16:44:26 +0530

    final code

--- main.c
+++ main.c
@@ -1,181 +1,79 @@
-#include <stdio.h>
-  
-// Function that returns the index of the
-// day for date DD/MM/YYYY
-int dayNumber(int day, int month, int year)
-{
-  
-    static int t[] = { 0, 3, 2, 5, 0, 3,
-                       5, 1, 4, 6, 2, 4 };
-    year -= month < 3;
-    return (year + year / 4
-            - year / 100
-            + year / 400
-            + t[month - 1] + day)
-           % 7;
+#include<stdio.h>
+
+#define CANDIDATE_COUNT
+
+#define CANDIDATE1 "Candidate A"
+#define CANDIDATE2 "Candidate B"
+#define CANDIDATE3 "Candidate C"
+#define CANDIDATE4 "Candidate D"
+
+int votesCount1=0,votesCount2=0,votesCount3=0,votesCount4=0,spoiledtvotes=0;
+void castVote(){
+int choice;    
+printf("\n\n ### Please choose your Candidate ####\n\n");
+printf("\n 1. %s",CANDIDATE1);
+printf("\n 2. %s",CANDIDATE2);
+printf("\n 3. %s",CANDIDATE3);
+printf("\n 4. %s",CANDIDATE4);
+printf("\n 5. %s","None of These");
+printf("\n\n Input your choice (1 - 4) : ");
+scanf("%d",&choice);
+switch(choice){
+    case 1: votesCount1++; break;
+    case 2: votesCount2++; break;
+    case 3: votesCount3++; break;
+    case 4: votesCount4++; break;
+    case 5: spoiledtvotes++; break;
+    default: printf("\n Error: Wrong Choice !! Please retry");
+             getchar();
 }
-  
-// Function that returns the name of the
-// month for the given month Number
-// January - 0, February - 1 and so on
-char* getMonthName(int monthNumber)
-{
-    char* month;
-  
-    switch (monthNumber) {
-    case 0:
-        month = "January";
-        break;
-    case 1:
-        month = "February";
-        break;
-    case 2:
-        month = "March";
-        break;
-    case 3:
-        month = "April";
-        break;
-    case 4:
-        month = "May";
-        break;
-    case 5:
-        month = "June";
-        break;
-    case 6:
-        month = "July";
-        break;
-    case 7:
-        month = "August";
-        break;
-    case 8:
-        month = "September";
-        break;
-    case 9:
-        month = "October";
-        break;
-    case 10:
-        month = "November";
-        break;
-    case 11:
-        month = "December";
-        break;
-    }
-    return month;
+printf("\n thanks for vote !!");
 }
-  
-// Function to return the number of days
-// in a month
-int numberOfDays(int monthNumber, int year)
-{
-    // January
-    if (monthNumber == 0)
-        return (31);
-  
-    // February
-    if (monthNumber == 1) {
-        // If the year is leap then Feb
-        // has 29 days
-        if (year % 400 == 0
-            || (year % 4 == 0
-                && year % 100 != 0))
-            return (29);
-        else
-            return (28);
-    }
-  
-    // March
-    if (monthNumber == 2)
-        return (31);
-  
-    // April
-    if (monthNumber == 3)
-        return (30);
-  
-    // May
-    if (monthNumber == 4)
-        return (31);
-  
-    // June
-    if (monthNumber == 5)
-        return (30);
-  
-    // July
-    if (monthNumber == 6)
-        return (31);
-  
-    // August
-    if (monthNumber == 7)
-        return (31);
-  
-    // September
-    if (monthNumber == 8)
-        return (30);
-  
-    // October
-    if (monthNumber == 9)
-        return (31);
-  
-    // November
-    if (monthNumber == 10)
-        return (30);
-  
-    // December
-    if (monthNumber == 11)
-        return (31);
+void votesCount(){
+printf("\n\n ##### Voting Statics ####");
+printf("\n %s - %d ", CANDIDATE1, votesCount1);
+printf("\n %s - %d ", CANDIDATE1, votesCount2);
+printf("\n %s - %d ", CANDIDATE1, votesCount3);
+printf("\n %s - %d ", CANDIDATE1, votesCount4);
+printf("\n %s - %d ", "Spoiled Votes", spoiledtvotes); 
 }
-  
-// Function to print the calendar of
-// the given year
-void printCalendar(int year)
-{
-    printf("     Calendar - %d\n\n", year);
-    int days;
-  
-    // Index of the day from 0 to 6
-    int current = dayNumber(1, 1, year);
-  
-    // i for Iterate through months
-    // j for Iterate through days
-    // of the month - i
-    for (int i = 0; i < 12; i++) {
-        days = numberOfDays(i, year);
-  
-        // Print the current month name
-        printf("\n ------------%s-------------\n",
-               getMonthName(i));
-  
-        // Print the columns
-        printf(" Sun   Mon  Tue  Wed  Thu  Fri  Sat\n");
-  
-        // Print appropriate spaces
-        int k;
-        for (k = 0; k < current; k++)
-            printf("     ");
-  
-        for (int j = 1; j <= days; j++) {
-            printf("%5d", j);
-  
-            if (++k > 6) {
-                k = 0;
-                printf("\n");
-            }
-        }
-  
-        if (k)
-            printf("\n");
-  
-        current = k;
-    }
-  
-    return;
+void getLeadingCandidate(){
+    printf("\n\n  #### Leading Candiate ####\n\n");
+    if(votesCount1>votesCount2 && votesCount1>votesCount3 && votesCount1 >votesCount4)
+    printf("[%s]",CANDIDATE1);
+    else if (votesCount2>votesCount3 && votesCount2>votesCount4 && votesCount2 >votesCount1)
+    printf("[%s]",CANDIDATE2);
+    else if(votesCount3>votesCount4 && votesCount3>votesCount2 && votesCount3 >votesCount1)
+    printf("[%s]",CANDIDATE3);
+    else if(votesCount4>votesCount1 && votesCount4>votesCount2 && votesCount4 >votesCount3)
+    printf("[%s]",CANDIDATE4);
+    else
+    printf("----- Warning !!! No-win situation----");    
+    
+    
+    
 }
-  
-// Driver Code
+
 int main()
 {
-    int year = 2016;
-  
-    // Function Call
-    printCalendar(year);
-    return 0;
+int i;
+int choice;
+do{
+printf("\n\n ###### ELECTION #####");
+printf("\n\n 1. Cast the Vote");
+printf("\n 2. Find Vote Count");
+printf("\n 3. Find leading Candidate");
+printf("\n 0. Exit");
+printf("\n\n Please enter your choice : ");
+scanf("%d", &choice);
+switch(choice)
+{
+case 1: castVote();break;
+case 2: votesCount();break;
+case 3: getLeadingCandidate();break;
+default: printf("\n Error: Invalid Choice");
+}
+}while(choice!=0);
+getchar();
+return 0;
 }
